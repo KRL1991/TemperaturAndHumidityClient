10eb8955a89865f38570d603a60002995e1cd63f
KRL1991 <kimx4706@edu.easj.dk>
2021-09-15 16:54:14 +0200

    Working Client that can connect to a RasberryPi and receive data

---
+++ src/Client.java
@@ -0,0 +1,65 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+
+
+    public static void main(String[] args) throws IOException {
+
+        String host = "10.200.130.31";
+        int port = 4501;
+        String temp;
+        String humd;
+
+        Scanner inputScanner = new Scanner(System.in);
+
+        try {
+            Socket socket = new Socket(host, port);
+
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+
+
+
+            new Thread(() -> {
+
+                while (true) {
+                    DataInputStream inputStream;
+
+
+                    try {
+                        inputStream = new DataInputStream(socket.getInputStream());
+
+                        // if (inputStream.readUTF().contains("C")) {
+
+                        // temp =  inputStream.readUTF();
+
+                        System.out.println(inputStream.readUTF());
+                        // }
+
+                        // if (inputStream.readUTF().contains("%")) {
+                        System.out.println(inputStream.readUTF());
+                        // }
+
+
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+            }).start();
+
+
+        } catch (Exception exception) {
+        }
+
+
+    }
+
+
+}
